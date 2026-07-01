90306dc8c9ac2f42b3c490cb2996584112618542
jecortes2406 <jecortes2406@gmail.com>
2026-06-30 23:04:10 -0400

    Update database_manager.py

--- database_manager.py
+++ database_manager.py
@@ -6,3 +6,10 @@ def obtener_todos_productos():
     data = c.fetchall()
     conn.close()
     return data
+def actualizar_stock(producto_id, cantidad_vendida):
+    conn = sqlite3.connect('gc_erp.db')
+    c = conn.cursor()
+    # Restamos el stock actual menos lo vendido
+    c.execute("UPDATE productos SET stock = stock - ? WHERE id = ?", (cantidad_vendida, producto_id))
+    conn.commit()
+    conn.close()
