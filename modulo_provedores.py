a0df87d059d17b20af2954f2f50197e0079a323c
jecortes2406 <jecortes2406@gmail.com>
2026-06-30 22:34:28 -0400

    Update and rename modulo_provedores.py.txt to modulo_provedores.py

---
+++ modulo_provedores.py
@@ -0,0 +1,7 @@
+def render_modulo_proveedores():
+    st.markdown("## 🏢 GESTIÓN DE PROVEEDORES")
+    st.markdown('<div class="card-white">', unsafe_allow_html=True)
+    # Aquí puedes añadir los campos: Nombre Empresa, Rif, Contacto, Categoría de Suministro
+    # Esto te permitirá enlazar el producto con quien te lo vende.
+    st.write("Módulo de catálogo de proveedores activo.")
+    st.markdown('</div>', unsafe_allow_html=True)
