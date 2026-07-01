38ff25ec468bb64f2b902d858ff94a2582ac793b
jecortes2406 <jecortes2406@gmail.com>
2026-07-01 03:20:18 -0400

    Update database_manager.py

--- database_manager.py
+++ database_manager.py
@@ -4,6 +4,8 @@ import pandas as pd
 def init_db():
     if 'db_inventario' not in st.session_state:
         st.session_state.db_inventario = pd.DataFrame(columns=[
-            'Código', 'Producto', 'Categoría', 'Costo USD', 'Precio Detal', 'Precio Mayor', 'Stock'
+            'Código', 'Producto', 'Categoría', 'Costo USD', 'Margen %', 'Comisión %', 'Precio Venta (Bs)'
         ])
+    if 'db_ventas' not in st.session_state:
+        st.session_state.db_ventas = pd.DataFrame()
     return st.session_state.db_inventario
